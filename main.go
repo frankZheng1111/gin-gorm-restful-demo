1055b7c2e284aebcef9d0d49ab30d27f246c4aa4
zhengdingyuan <dingyuan.zheng@facialcloud.com>
2018-04-24 18:33:56 +0800

    fmt code

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-  "gin-gorm-restful-demo/routers"
-  "github.com/gin-gonic/gin"
+	"gin-gorm-restful-demo/routers"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  router := gin.Default()
+	router := gin.Default()
 
-  v1 := router.Group("/v1")
-  routers.InitRouters(v1)
+	v1 := router.Group("/v1")
+	routers.InitRouters(v1)
 
-  router.Run() // 8080
+	router.Run() // 8080
 }
--- routers/routers.go
+++ routers/routers.go
@@ -1,79 +1,79 @@
 package routers
 
 import (
-  "fmt"
-  "github.com/gin-gonic/gin"
-  "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/sqlite"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var db *gorm.DB
 var err error
 
 type Person struct {
-  ID uint `json:"id"`
-  FirstName string `json:"first_name"`
-  LastName string `json:"last_name"`
+	ID        uint   `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
 
 func InitRouters(routerVersionGroup *gin.RouterGroup) {
-  db, err = gorm.Open("sqlite3", "./db/gorm.db")
-  if err != nil {
-    fmt.Println(err)
-  }
+	db, err = gorm.Open("sqlite3", "./db/gorm.db")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-  db.AutoMigrate(&Person{})
+	db.AutoMigrate(&Person{})
 
-  // 连接池最小连接数
-  //
-  db.DB().SetMaxIdleConns(10)
-  // 连接池最大连接数
-  //
-  db.DB().SetMaxOpenConns(100)
+	// 连接池最小连接数
+	//
+	db.DB().SetMaxIdleConns(10)
+	// 连接池最大连接数
+	//
+	db.DB().SetMaxOpenConns(100)
 
-  persons := routerVersionGroup.Group("/persons")
-  persons.GET("/", getPersons)
-  persons.GET("/:id", getPerson)
-  persons.POST("/", createPerson)
-  persons.PUT("/:id", updatePerson)
+	persons := routerVersionGroup.Group("/persons")
+	persons.GET("/", getPersons)
+	persons.GET("/:id", getPerson)
+	persons.POST("/", createPerson)
+	persons.PUT("/:id", updatePerson)
 }
 
 func updatePerson(c *gin.Context) {
-  var person Person
-  id := c.Params.ByName("id")
-  if err := db.Where("id = ?", id).First(&person).Error; err != nil {
-    c.AbortWithStatus(404)
-    fmt.Println(err)
-  }
-  c.BindJSON(&person)
-  db.Save(&person)
-  c.JSON(200, person)
+	var person Person
+	id := c.Params.ByName("id")
+	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	}
+	c.BindJSON(&person)
+	db.Save(&person)
+	c.JSON(200, person)
 }
 
 func createPerson(c *gin.Context) {
-  var person Person
-  c.BindJSON(&person)
-  db.Create(&person)
-  c.JSON(200, person)
+	var person Person
+	c.BindJSON(&person)
+	db.Create(&person)
+	c.JSON(200, person)
 }
 
 func getPerson(c *gin.Context) {
-  id := c.Params.ByName("id")
-  var person Person
-  if err := db.Where("id = ?", id).First(&person).Error; err != nil {
-    c.AbortWithStatus(404)
-    fmt.Println(err)
-  } else {
-    c.JSON(200, person)
-  }
+	id := c.Params.ByName("id")
+	var person Person
+	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, person)
+	}
 }
 
 func getPersons(c *gin.Context) {
-  var people []Person
-  if err := db.Find(&people).Error; err != nil {
-    c.AbortWithStatus(404)
-    fmt.Println(err)
-  } else {
-    c.JSON(200, people)
-  }
+	var people []Person
+	if err := db.Find(&people).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, people)
+	}
 }
